37c49c36ace27104aadd6f473a7ab10c7067687e
Aharshraj Gunasekaran <aharsh191197@gmail.com>
2020-06-15 19:22:35 +0530

    firstCommit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// import and configure firebase
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: ["AIzaSyCZqNCTiUXjMcmeFa9vVko4_IlulWmOWbc"],
+  authDomain: ["1:372906331410:android:0289efb7726855c8c477cb"],
+  databaseURL: ["1:372906331410:android:0289efb7726855c8c477cbhttps://socialapp-20bcc.firebaseio.com"],
+  storageBucket: ["socialapp-20bcc.appspot.com"],
+}
+export const firebaseApp = firebase.initializeApp(firebaseConfig)
